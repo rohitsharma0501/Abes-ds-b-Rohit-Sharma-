d6ec7e349c3e2b809c7278da4a9561e30192ca02
Rohit Dubey <rohitsharma05012006@gmail.com>
2025-10-14 11:04:54 +0530

    dom

---
+++ js/ES6/dom.js
@@ -0,0 +1,6 @@
+const div = document.createElement("div") ;
+const h1 = document.createElement("h1") ;
+h1.innerText="Python is BEST !!" ;
+div.appendChild(h1) ;
+const id = document.getElementById("root") ;
+id.appendChild(div) ;
