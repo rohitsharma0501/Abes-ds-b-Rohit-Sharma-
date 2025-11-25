c4452400f5b933c24a935f2d6ddef2be914c6834
Rohit Dubey <rohitsharma05012006@gmail.com>
2025-11-25 11:26:07 +0530

    server request response

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,3 +2,14 @@
 const express = require('express') ;
 const app = express() ;
 const port = 8080;
+
+app.get('/',(req,res)=>{
+    res.send("This is my first server") ;
+});
+app.get('/about',(req,res)=>{
+    res.send("This is my first about server") ;
+});
+
+app.listen(port,()=>{
+    console.log(`server is run at:http://localhost:${port}`) ;
+}) ;
