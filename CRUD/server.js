4cd92a596a49ebaf2f731860b60778148e58b80c
Rohit Dubey <rohitsharma05012006@gmail.com>
2025-11-29 11:04:06 +0530

    middleware

--- CRUD/server.js
+++ CRUD/server.js
@@ -2,6 +2,17 @@ import express from 'express' ;
 const app = express() ;
 const port = 4041 ;
 
+//middleware
+app.use(express.json())
+
+// create array for store data
+const students=[
+    {
+        id:1,
+        nmae:"Aditya",
+        class:"b.tech"
+    }
+]
 
 app.listen(port,()=>{
     console.log(`server is run at:http://localhost:${port}`) ;
